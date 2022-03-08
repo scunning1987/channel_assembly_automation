4b905aac1ddb6a86cb1d927fe153d9e4523c5c7e
scott <cunsco@amazon.com>
2022-03-08 16:52:22 -0700

    Tue Mar  8 23:52:22 UTC 2022

--- ca_automation_status_check.py
+++ ca_automation_status_check.py
@@ -1,8 +1,13 @@
 import json
+import logging
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    LOGGER.info(event)
     # TODO implement
     return {
         'statusCode': 200,
-        'body': event
+        'body': json.dumps(event)
     }
