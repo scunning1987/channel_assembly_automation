ea64032062a24522895f32e0d6f2285e9a4a1a1a
scott <cunsco@amazon.com>
2022-03-09 21:17:24 -0700

    Thu Mar 10 04:17:24 UTC 2022

--- ca_automation_api_handler.py
+++ ca_automation_api_handler.py
@@ -9,6 +9,19 @@ LOGGER.setLevel(logging.INFO)
 def lambda_handler(event, context):
     LOGGER.info(event)
 
+    evt_client = boto3.client('events')
+
+    response = evt_client.create_event_bus(
+        Name='string',
+        EventSourceName='string',
+        Tags=[
+            {
+                'Key': 'string',
+                'Value': 'string'
+            },
+        ]
+    )
+
 
     # boto3 client initialization
     emt_client = boto3.client('mediatailor')
@@ -37,10 +50,18 @@ def lambda_handler(event, context):
         paginator = emt_client.get_paginator('list_channels')
 
         response_iterator = paginator.paginate().build_full_result()
+        response_json = json.loads(json.dumps(response_iterator, default = lambda o: f"<<non-serializable: {type(o).__qualname__}>>"))
 
-        return response_iterator
+        pretty_channel_json = dict()
 
-    return str(mediatailor_get_channels())
+        if len(response_json['Items']) > 0:
+            for channel_item in response_json['Items']:
+                channel_name = channel_item['ChannelName']
+                channel_state = channel_item['ChannelState']
+                channel_outputs = channel_item['Outputs']
+                pretty_channel_json[channel_name] = {"channel_state":channel_state,"outputs":channel_outputs}
+
+        return pretty_channel_json
 
     try:
         # Path in API call
@@ -50,7 +71,8 @@ def lambda_handler(event, context):
         request_method = event['httpMethod']
 
         # Body in API Call
-        request_body = event['body']
+        request_body = json.loads(event['body'])
+
     except Exception as e:
         LOGGER.error("Unable to extract url path, request method, or body from request payload")
         response_json = {"status":"Unable to extract url path, request method, or body from request payload"}
@@ -62,30 +84,33 @@ def lambda_handler(event, context):
             response_json = {"status":"For this API resource, we expect HTTP PUT with json payload of the playlist, %s" % (request_method)}
             return api_response(500,response_json)
 
-        if request_body is None or isinstance(request_body,dict) != False:
+        LOGGER.info("request body is of type: %s " % (type(request_body)))
+        if not isinstance(request_body,dict):
             response_json = {"status":"For this API resource, we expect a json payload"}
             return api_response(500,response_json)
 
 
         ## See if we can parse Title from json
-        # uuid.uuid4().hex
+        # if we cannot get channel name, create one using a uuid . uuid.uuid4().hex
         try:
             channel_name = request_body['PlayoutChannel']
+            if channel_name is None:
+                channel_name = ""
         except Exception as e:
             random_uuid = uuid.uuid4().hex
             LOGGER.info("Could not get channel name from playlist, creating new channel: %s " % (random_uuid))
             channel_name = random_uuid
 
-        if len(channel_name) < 1 or channel_name is None:
+        if len(channel_name) < 1:
             random_uuid = uuid.uuid4().hex
             LOGGER.info("Could not get channel name from playlist, creating new channel: %s " % (random_uuid))
             channel_name = random_uuid
 
-        # Create DB entry for channel
-        # Check if channel exists, if not, create
-
 
         # Create DB entry for API request
+        request_uuid = uuid.uuid4().hex
+
+        create_api_req_record(request_uuid)
 
         # Send custom EventBridge to start Step workflow
         # send this json structure:
